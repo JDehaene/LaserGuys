3e548bb983e1eee372e68f07b65671306ef0987b
Kair0z <52052044+Kair0z@users.noreply.github.com>
2020-07-10 12:51:16 +0200

    First scripts
    - Added PlayerController
    - Added CameraBehaviour
    - Added InputManager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Vector2 LThumb(int playerID)
+    {
+        return new Vector2(Input.GetAxis("HorizontalL_P" + playerID), Input.GetAxis("VerticalL_P" + playerID));
+    }
+
+    public Vector2 RThumb(int playerID)
+    {
+        return new Vector2(Input.GetAxis("HorizontalR_P" + playerID), Input.GetAxis("VerticalR_P" + playerID));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private InputManager _input;
+
+    private void Awake()
+    {
+        _input = FindObjectOfType<InputManager>();
+        if (!_input) { };
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        Vector2 move = _input.LThumb(1) * 10;
+        Debug.Log("Input: " + move);
+        gameObject.transform.Translate(new Vector3(move.x, 0, move.y), Space.World);
+    }
+}
